f813c2045bf5c287a8af234c33175c80355e03eb
yizheng <YiZheng.Yang@linux.alibaba.com>
2026-06-11 17:30:10 +0800

    chore(sec-core): bump version to v0.6.1
    Signed-off-by: yizheng <YiZheng.Yang@linux.alibaba.com>

--- src/agent-sec-core/agent-sec-cli/src/agent_sec_cli/cli.py
+++ src/agent-sec-core/agent-sec-cli/src/agent_sec_cli/cli.py
@@ -31,7 +31,7 @@ try:
 
     __version__ = get_version("agent-sec-cli")
 except Exception:
-    __version__ = "0.6.0"  # pragma: no cover
+    __version__ = "0.6.1"  # pragma: no cover
 
 app = typer.Typer(
     name="agent-sec-cli",
